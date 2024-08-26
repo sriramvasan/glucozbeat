64ccb327643472c323334f4085b0976207f034ee
sriramvasan <svas0011@student.monash.edu>
2024-08-26 15:29:44 +1000

    minor changes 4

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
         />
       </div>
       <div>
-        Minor Changes 3.0
+        Minor Changes 4.0
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
