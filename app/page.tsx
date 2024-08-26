4a90ecdefc414770abff67977b542b1b74b7a52b
sriramvasan <svas0011@student.monash.edu>
2024-08-26 14:30:57 +1000

    tiral changes1

--- app/page.tsx
+++ app/page.tsx
@@ -38,6 +38,9 @@ export default function Home() {
           priority
         />
       </div>
+      <div>
+        Minor Changes 1.
+      </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
