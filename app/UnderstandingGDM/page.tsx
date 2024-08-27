7a0978704404292598a2b9ded40c634f09a890ce
sriramvasan <svas0011@student.monash.edu>
2024-08-27 11:49:00 +1000

    adding understanding GDM Page

--- app/UnderstandingGDM/page.tsx
+++ app/UnderstandingGDM/page.tsx
@@ -1,11 +1,65 @@
+'use client';
 import React from 'react'
+import Accordion from '../Components/Accordian'
+import { ImageCard } from '../Components/ImageCard';
+
+const MythData = [
+  { 
+      id: 1,
+      title : "Myth 1: Eating too much sugar causes GDM.",
+      content: "Yes, diet matters, but GDM isn’t just about what you eat—it’s influenced by things like genetics, age, and hormones. It’s a common myth that eating sugar alone causes GDM. The real issue is more about overall energy balance and weight gain during pregnancy, not specific foods. So while it’s important to watch your carbs, they aren’t the enemy—it’s about finding the right balance that works for you."
+  },
+  {   
+      id: 2, 
+      title : "Myth 2: I will need insulin to manage my gestational diabetes.",
+      content: "Don't worry, most women with gestational diabetes don’t need insulin. The first step is usually making small changes to your diet and getting some regular exercise, which can often keep your blood sugar in check. Your body is already working hard to produce more insulin during pregnancy, and these tweaks can help it do its job. Insulin can be a great tool if needed, but for most women, it’s not necessary—only about 10-20% actually end up needing it."
+  },
+  {
+    id: 3,
+    title : "Myth 3: All women with GDM will have big babies.",
+    content: "Having a big baby is a concern with GDM, but it’s not a given. The risk comes from higher blood sugar levels passing from mom to baby, leading to extra fat storage in the baby. But the good news is, by keeping your blood sugar in check through diet and exercise, you can greatly lower the chances of having a large baby. It’s all about managing those sugar levels, and with the right plan, you can have a healthy pregnancy and baby."
+  },
+  {
+    id:4,
+    title : "Myth 4: Addressing the Fear Section",
+    content: "GDM is a result of complex interactions between hormones, genetics, and other factors beyond your control. What matters most is how you manage it with the right knowledge and support"
+  }
+]
+
 
 const UnderstandingGDM = () => {
   return (
-    <div>
-      The Quiz Page
+    <>
+    <div>s
+      <h1 className='m-8 px-8 '>Debunk the Rumors: Understanding GDM </h1>
+      <p className='p-8 m-8'> Let’s explore common myths about gestational diabetes and see what the data says.</p>
     </div>
+    <div className='hide-scrollbar flex h-[550px] m-5 p-5' >
+      <ImageCard 
+      backgroundImage='/priya-persona-2.jpg' 
+      title='Meet Priya, a first-time mom in her early 30s.' 
+      subtitle='When Priya was diagnosed with GDM, She was worried she did something wrong—maybe she ate too much sugar? But is that really the cause? '
+      src = "/menu.svg"></ImageCard>
+    </div>
+
+    <div className='m-5 p-5'>
+    {MythData.map((data) =>(
+      <Accordion key= {data.id} title={data.title} content={data.content}></Accordion>
+      ))}
+    </div>
+
+    <div className='p-5 m-5'>
+      <h1>Relating Back to Priya’s Experience</h1>
+      <p className=''>For Priya, understanding that GDM isn’t her fault was a relief. She focused on simple steps like balanced meals and moderate exercise, which helped keep her and her baby healthy</p>
+    </div>
+
+    <div className='p-5 m-5'>
+      <p>Remember: You are not alone. Like Priya, you can take control of your GDM journey with the right knowledge and support. Explore our resources to find meal plans and exercises tailored to your needs.</p>
+    </div>
+    </>
   )
 }
 
 export default UnderstandingGDM
+
+{/* <a href="https://www.freepik.com/free-photo/smiling-young-beautiful-girl-wearing-white-t-shirt-points-isolated-purple-with-copy-space_13958905.htm#fromView=search&page=1&position=7&uuid=7114e19b-5bd4-4d7c-9d9b-01f456cda07c">Image by stockking on Freepik</a> */}
--- app/Women/page.tsx
+++ app/Women/page.tsx
@@ -1,53 +1,23 @@
 
 import { PEOPLE_URL } from "../Constants";
 import Image from "next/image";
-
-interface FeatureProps {
-  backgroundImage: string;
-  title: string;
-  subtitle: string;
-}
-
-const Feature = ({ backgroundImage, title, subtitle}: FeatureProps) => {
-  return (
-    <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
-    style={{
-      backgroundImage: `url(${backgroundImage})`,
-    }}
-    >
-     <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
-     <div className="flexCenter gap-4">
-        <div className="rounded-full bg-purple-300 p-4">
-          <Image
-            src="/folded-map.svg"
-            alt="map"
-            width={28}
-            height={28}
-          />
-        </div>
-        <div className="flex flex-col gap-1">
-        <h4 className="bold-18 text-white">{title}</h4>
-        <p className="regular-14 text-white">{subtitle}</p>
-        </div>
-      </div>
-     </div>
-    </div>
-  )
-}
+import { ImageCard } from "../Components/ImageCard";
 
 const Women = () => {
   return (
     <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
       <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
-        <Feature 
+        <ImageCard 
           backgroundImage="/homepage-bg-1.png"
           title="GDM Myth-Busting"
           subtitle="Manage Your Health with Confidence"
+          src = "/folded-map.svg"
         />
-        <Feature
+        <ImageCard
           backgroundImage="/homepage-bg.png"
           title="Healthy Eating Guide"
           subtitle="Personalized GDM Resources"
+          src = "/folded-map.svg"
         />
       </div>
 
