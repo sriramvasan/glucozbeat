987a640f4f385635581b1113084caabbed958660
sriramvasan <svas0011@student.monash.edu>
2024-08-29 15:32:26 +1000

    Implementing Flipcards

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -6,7 +6,7 @@ const About = () => {
     <div >
       Welcome to the About us Page
       <div className='space-x-10 mx-auto justify-start'>
-        <FlipCard heading = 'a' content='b' />
+        
       </div>
       
     </div>
--- app/Components/FlipCard/FlipCard.tsx
+++ app/Components/FlipCard/FlipCard.tsx
@@ -1,35 +1,45 @@
 'use client';
 import React, { useState } from 'react'
 import ReactCardFlip from 'react-card-flip';
+import styles from './FlipCard.module.css';
 
 interface flipCardItem {
 
   heading : string,
   content: string, 
   image? :string,
-  
+  key?: number,
+  className?: string
 }
 
-const FlipCard = ({heading, content, image }: flipCardItem) => {
+const FlipCard = ({heading, content, image, key, className }: flipCardItem) => {
 const [flip, setFlip] = useState(false);
   return (
-    <div className=''>
+    <div className={`${className} ${styles.position}`}>
       <ReactCardFlip isFlipped={flip} 
       flipDirection='vertical' 
       flipSpeedBackToFront={0.5} 
       flipSpeedFrontToBack={0.5} >
-        <div className='bg-purple-600 text-white w-5/12 h-72 rounded-lg text-center p-5 m-5' onClick={() => setFlip(!flip)}>
-            Welcome to GDM Teams.
-            <div></div>
-            <br />
-            <br />
-        </div>
-        <div className='p-5 m-5 text-center bg-purple-600 text-white rounded-lg  w-5/12 h-72' onClick={() => setFlip(!flip)}>
-                Computer Science Portal.
-                <br />
-                <br />
-
+        {/* Front of the card */}
+        <div className='bg-purple-600 rounded-lg w-5/12 h-80 text-white text-justify p-5 m-5 ' onClick={() => setFlip(!flip)}>
+            <h2 className={`sm:text-[20px] ${styles.heading2}`}>
+                Myth {key}
+            </h2>
+            <hr className=' px-2 mx-2 ' />
+            <div className={styles.content}>
+            {heading}
             </div>
+        </div>
+        {/* Back of the card */}
+        <div className='p-5 m-5 text-justify bg-purple-600 text-white rounded-lg w-5/12 h-80' onClick={() => setFlip(!flip)}>
+            <h2 className={`md:text-[20px] sm:text-[15px] ${styles.heading2}`}>
+                {heading}
+            </h2>
+            <hr className=' px-2 mx-2 md:px-1 md:mx-1' />
+            <div className={`md:text-[15px] sm:text-[10px] ${styles.content}`}>
+                {content}
+            </div>
+        </div>
       </ReactCardFlip>
     </div>
 
